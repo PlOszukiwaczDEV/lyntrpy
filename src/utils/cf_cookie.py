82ae7e33e954dfc31a39047082be905fbeef1400
PlOszukiwacz <87446593+PlOszukiwaczDEV@users.noreply.github.com>
2024-08-08 20:09:14 +0200

    guys cloudflare bypass real

--- src/utils/cf_cookie.py
+++ src/utils/cf_cookie.py
@@ -1,1 +1,20 @@
-# ToDo: implement: https://github.com/zfcsoftware/cf-clearance-scraper (as an option)
+import requests
+import json
+
+CF_COOKIE_ON = False # if you want to turn on you need to setup https://github.com/zfcsoftware/cf-clearance-scraper
+
+def get_cf_cookie():
+    if CF_COOKIE_ON:
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        data = {
+            "url": "https://lyntr.com",
+            "mode": 'waf'
+        }
+
+        r = requests.post("http://localhost:3000/cf-clearance-scraper", headers=headers, data=json.dumps(data))
+        return r
+    else:
+        return "Disabled"
